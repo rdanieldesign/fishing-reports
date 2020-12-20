ffc175f61ca1ffd723a1bebef018b939fc89c98d
rdanieldesign <rdanieldesign@gmail.com>
2020-12-20 10:42:56 -0500

    pw config file

--- auth/auth-action.ts
+++ auth/auth-action.ts
@@ -1,9 +1,8 @@
 import { AuthPrompt } from './auth-prompt';
 import { MySQLService } from '../mysql-service';
+import { MYSQL_PASSWORD } from '../credentials';
 const mySQLService = new MySQLService();
-const authPrompt = new AuthPrompt();
 
 export async function login(): Promise<void> {
-  const answer = await authPrompt.getPassword();
-  mySQLService.login(answer.password);
+  mySQLService.login(MYSQL_PASSWORD);
 };
